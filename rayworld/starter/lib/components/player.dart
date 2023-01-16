f42297f73581760dd2525a9be4e120457df86c86
CHOIHEEJAE <gmlwochl123@gmail.com>
2023-01-16 14:45:29 +0900

    player add

--- rayworld/starter/lib/components/player.dart
+++ rayworld/starter/lib/components/player.dart
@@ -1,5 +1,11 @@
 import 'package:flame/components.dart';
 
+/*
+SpriteComponent는 spriteSheet(게임 ui 리소스) 의 기본이 되는 컴포넌트이다.
+특정한 이미지를 가져와 게임 화면이라는 도화지에 그린다고 생각하면 편하다.
+
+HasGameRef를 추가하면 모든 컴포넌트가 게임 루프에 접근할 수 있습니다.
+ */
 class Player extends SpriteComponent with HasGameRef {
   Player()
       : super(
